ddd31c470b11e5294309bd9fa0699d2f82fba1fa
fuad ab <fuadabdela95@gmail.com>
2024-10-26 07:52:35 +0300

    initial commit

--- JavaApplication5/src/javaapplication5/JavaApplication5.java
+++ JavaApplication5/src/javaapplication5/JavaApplication5.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package library.management.system;
+package javaapplication5;
 
 /**
  *
  * @author kiot
  */
-public class LibraryManagementSystem {
+public class JavaApplication5 {
 
     /**
      * @param args the command line arguments
