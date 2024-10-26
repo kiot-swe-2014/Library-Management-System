c2e722e5bbe5e24a7f827c8b62975750260177ba
fuad ab <fuadabdela95@gmail.com>
2024-10-26 07:58:30 +0300

    initialize of project

--- Library Management System/src/library/management/system/LibraryManagementSystem.java
+++ Library Management System/src/library/management/system/LibraryManagementSystem.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package javaapplication5;
+package library.management.system;
 
 /**
  *
  * @author kiot
  */
-public class JavaApplication5 {
+public class LibraryManagementSystem {
 
     /**
      * @param args the command line arguments
